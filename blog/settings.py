f9838ea2c7711f9c626024e85030e92b341b41d6
habib <habib.abdelgaber@yahoo.com>
2021-06-19 20:13:27 +0300

    added updated

--- blog/settings.py
+++ blog/settings.py
@@ -3,12 +3,12 @@ import os
 ENV = 'env'
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    DATABASE_URL = 'postgres://jltzdmyzzsimhy:2f8bf8e6a70719612d0f6c31de5720eec37b6d940e0abf0eeebffc6a7d451198@ec2-50-17-255-120.compute-1.amazonaws.com:5432/d3dc2rngdlrnp5'
-    # if ENV == 'env':
-    #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # else:
-    #     debug = False
-    #     SQLALCHEMY_DATABASE_URI = 'postgres://jltzdmyzzsimhy:2f8bf8e6a70719612d0f6c31de5720eec37b6d940e0abf0eeebffc6a7d451198@ec2-50-17-255-120.compute-1.amazonaws.com:5432/d3dc2rngdlrnp5'
+    # DATABASE_URL = 'postgres://jltzdmyzzsimhy:2f8bf8e6a70719612d0f6c31de5720eec37b6d940e0abf0eeebffc6a7d451198@ec2-50-17-255-120.compute-1.amazonaws.com:5432/d3dc2rngdlrnp5'
+    if ENV == 'env':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    else:
+        debug = False
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
        
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     debug = os.environ.get('debug')
