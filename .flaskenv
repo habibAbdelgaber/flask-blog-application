1d9267bf0a47e2e8674459380c5b95010c27fde3
habib <habib.abdelgaber@yahoo.com>
2021-06-20 20:20:17 +0300

    flask blog app updated

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
 FLASK_APP=blog
 FLASK_ENV=development
-set FLASK_APP=/path/to/automain.py
+
--- blog/settings.py
+++ blog/settings.py
@@ -4,16 +4,27 @@ ENV = 'env'
 # class Config(object):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 if ENV == 'env':
+    debug = os.environ.get('debug')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 else:
     debug = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://bihnkfojpagzyr:9d75b34e093a2129f2bf3c209eafde2b1a20f950aedcdbc198ee7c1c4302c351@ec2-35-170-85-206.compute-1.amazonaws.com:5432/d2v5ccb96j6f2i')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
-SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
-debug = os.environ.get('debug')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+# DEBUG = os.environ.get('DEBUG')
 MAIL_SERVER = os.environ.get('EMAIL_SERVER')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+
+# set FLASK_APP=/path/to/automain.py
+# DATABASE_URL='sqlite:///sqlite3'
+# SECRET_KEY='aff08866d4fcc1a574f0231f6ed0c14d'
+# EMAIL_SERVER='smtp.googlemail.com'
+# EMAIL_PORT=587
+# EMAIL_USERNAME='youremail@gmail.com'
+# EMAIL_PASSWORD='youremailpassword'
+# EMAIL_USE_TLS=True
+# EMAIL_USE_SSL=False
