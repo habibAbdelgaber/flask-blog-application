462f80787a25c444aae1675da3c73333cf7e0dc7
habib <habib.abdelgaber@yahoo.com>
2021-06-21 18:47:41 +0300

    flask blog application updated!

--- blog/settings.py
+++ blog/settings.py
@@ -1,16 +1,16 @@
 import os
 
-ENV = 'env'
-# class Config(object):
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if ENV == 'env':
-    debug = os.environ.get('debug')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-else:
-    debug = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://iiamjzfoyjyfzv:4fe9d4d61feaf1472d3bcbe22e71a5c5480bbdd7907f8f0c0276366a902c96cd@ec2-52-4-111-46.compute-1.amazonaws.com:5432/d9pru0dgmgik21')
-    
+# ENV = 'env'
+# # class Config(object):
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# if ENV == 'env':
+#     debug = os.environ.get('debug')
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# else:
+#     debug = False
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#     # SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://iiamjzfoyjyfzv:4fe9d4d61feaf1472d3bcbe22e71a5c5480bbdd7907f8f0c0276366a902c96cd@ec2-52-4-111-46.compute-1.amazonaws.com:5432/d9pru0dgmgik21')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # DEBUG = os.environ.get('DEBUG')
 MAIL_SERVER = os.environ.get('EMAIL_SERVER')
